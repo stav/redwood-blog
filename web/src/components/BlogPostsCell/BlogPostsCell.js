d100f5d8b1cf6620866659a7a12f7c7bab8f13ad
Steven Almeroth <sroth77@gmail.com>
2021-08-03 14:04:42 +0200

    post detail

--- web/src/components/BlogPostsCell/BlogPostsCell.js
+++ web/src/components/BlogPostsCell/BlogPostsCell.js
@@ -1,5 +1,7 @@
 // web/src/components/BlogPostsCell/BlogPostsCell.js
 
+import BlogPost from 'src/components/BlogPost'
+
 export const QUERY = gql`
   query BlogPostsQuery {
     posts {
@@ -20,13 +22,5 @@ export const Failure = ({ error }) => (
 )
 
 export const Success = ({ posts }) => {
-  return posts.map((post) => (
-    <article key={post.id}>
-      <header>
-        <h2>{post.title}</h2>
-      </header>
-      <p>{post.body}</p>
-      <div>Posted at: {post.createdAt}</div>
-    </article>
-  ))
+  return posts.map((post) => <BlogPost key={post.id} post={post} />)
 }
---
+++ web/src/pages/BlogPostPage/BlogPostPage.js
@@ -0,0 +1,11 @@
+// web/src/pages/BlogPostPage/BlogPostPage.js
+
+import BlogPostCell from 'src/components/BlogPostCell'
+
+const BlogPostPage = ({ id }) => {
+  return (
+    <BlogPostCell id={id} />
+  )
+}
+
+export default BlogPostPage
